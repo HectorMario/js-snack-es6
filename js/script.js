1442ae6bd8f4dfe60024b84b1d397ee421fa6ee9
HectorMario <hector.mario.peraz@gmail.com>
2023-03-27 09:59:48 +0200

    xd

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const byke = [
+    {
+        nome: "GW",
+        peso: "50 gr",
+    },
+    {
+        nome: "specialite",
+        peso: "40 gr"
+    },
+    {
+        nome: "santropel",
+        peso: "35 gr"
+    },
+    {
+        nome: "vespa",
+        peso: "55 gr"
+    }
+]
+
+function pesante (){
+    
+}
